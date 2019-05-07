bcde92bd5790316c51574d97401258eb7e1cd7a8
Charles Advincula <cadvincu@ucsc.edu>
2019-05-06 22:56:05 -0700

    Added comments for GameOver state methods + added .gitignore to dev branch

--- js/main.js
+++ js/main.js
@@ -142,17 +142,17 @@ var GameOver = function(game) {};
 GameOver.prototype = {
 	preload: function()
 	{
-		
+		console.log("GameOver: preload");
 	},
 
 	create: function()
 	{
-
+		console.log("GameOver: create");
 	},
 
 	update: function()
 	{
-		
+		console.log("GameOver: update");
 	}
 }
 
